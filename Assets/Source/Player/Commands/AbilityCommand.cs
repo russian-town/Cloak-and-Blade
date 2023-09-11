0e07ffb0a945eafb6543b0f82d2d8de99948fcff
Yuriy Golobokov <113376932+russian-town@users.noreply.github.com>
2023-09-11 12:24:14 +0600

    Add_command
    Add command

---
+++ Assets/Source/Player/Commands/AbilityCommand.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityCommand : Command
+{
+    public override void Cancel()
+    {
+    }
+
+    public override void Execute()
+    {
+    }
+}
