51b5585ab840406f56082516bcc0d7eeecfe90b0
Yuriy <russian_town_98@mail.ru>
2023-09-23 20:02:16 +0600

    Merge

--- Assets/Source/Game/Game.cs
+++ Assets/Source/Game/Game.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    [SerializeField] private GameObject _pauseScreen;
+    [SerializeField] private GameObject _playerUI;
+
     private Player _player;
     private Pause _pause;
 
@@ -22,11 +25,15 @@ public class Game : MonoBehaviour
     public void PauseGame() 
     {
         _pause.Enable();
+        _pauseScreen.SetActive(true);
+        _playerUI.SetActive(false);
     }
 
     public void Continue()
     {
         _pause.Disable();
+        _pauseScreen.SetActive(false);
+        _playerUI.SetActive(true);
     }
 
     public void GameOver() { }
