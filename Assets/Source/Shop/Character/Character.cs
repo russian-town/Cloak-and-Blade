af483b5783c1ef1c672a4f4671d7c087b5942e0e
Arphail <alexusoffd@gmail.com>
2023-10-08 16:48:27 +0300

    Shop ui ready

--- Assets/Source/Shop/Character/Character.cs
+++ Assets/Source/Shop/Character/Character.cs
@@ -9,7 +9,7 @@ public class Character : ScriptableObject
     [SerializeField] private Player _player;
     [SerializeField] private MenuModel _menuPlayerModelTemplate;
 
-    [SerializeField] private bool _isBuyed;
+    [SerializeField] private bool _isBought;
     [SerializeField] private bool _isSelect;
 
     private MenuModel _menuModel;
@@ -17,12 +17,12 @@ public class Character : ScriptableObject
     public Type Type => _type;
     public int Price => _price;
     public Sprite Icon => _icon;
-    public bool IsBuyed => _isBuyed;
+    public bool IsBuyed => _isBought;
     public bool IsSelect => _isSelect;
     public Player Player => _player;
     public MenuModel MenuModelTemplate => _menuPlayerModelTemplate;
 
-    public void Buy() => _isBuyed = true;
+    public void Buy() => _isBought = true;
 
     public void Select() => _isSelect = true;
 
