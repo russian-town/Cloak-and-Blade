e3455c3a088d6a80209ae1efc312c3b0e88289c2
Arphail <Alexusoffd@gmail.com>
2023-11-12 16:01:25 +0300

    Basic animation curve ready

--- Assets/Source/Player/PlayerModel.cs
+++ Assets/Source/Player/PlayerModel.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class PlayerModel : MonoBehaviour
 {
+    [SerializeField] private PlayerModel _modelToTransit;
+
     public bool Enabled => gameObject.activeInHierarchy;
 
     public void Hide() => gameObject.SetActive(false);
 
     public void Show() => gameObject.SetActive(true);
+
+    public void SwitchModels()
+    {
+        Hide();
+        _modelToTransit.Show();
+    }
 }
