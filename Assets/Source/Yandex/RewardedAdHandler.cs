019a0bb5d93f0f47129b5749fc69fb6be28912b3
Yuriy <russian_town_98@mail.ru>
2023-12-15 02:19:12 +0600

    Intermediate_changes

--- Assets/Source/Yandex/RewardedAdHandler.cs
+++ Assets/Source/Yandex/RewardedAdHandler.cs
@@ -1,59 +1,22 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class RewardedAdHandler : MonoBehaviour
 {
-    [SerializeField] private Button _playAdButton;
-    [SerializeField] private Button _dontPlayAdButton;
-    [SerializeField] private ScreenAnimationHandler _animationHandler;
-    [SerializeField] private PlayerView _playerView;
-
     private YandexAds _yandexAds;
     private Player _player;
     private CommandExecuter _commandExecuter;
 
-    private void OnEnable()
-    {
-        _playAdButton.onClick.AddListener(OnButtonClick);
-        _dontPlayAdButton.onClick.AddListener(Hide);
-        _yandexAds.RewardedCallback += OnRewardedVideoCallBack;
-    }
-
-    private void OnDisable()
-    {
-        _playAdButton.onClick.RemoveListener(OnButtonClick);
-        _dontPlayAdButton.onClick.RemoveListener(Hide);
-        _yandexAds.RewardedCallback -= OnRewardedVideoCallBack;
-    }
+    private void OnDisable() => _yandexAds.RewardedCallback -= OnRewardedVideoCallBack;
 
     public void Initialize(Player player, YandexAds yandexAds)
     {
         _yandexAds = yandexAds;
         _player = player;
         _commandExecuter = _player.CommandExecuter;
-        gameObject.SetActive(true);
+        _yandexAds.RewardedCallback += OnRewardedVideoCallBack;
     }
 
-    public void Show()
-    {
-        _animationHandler.ScreenFadeIn();
-        _playerView.Hide();
-    }
-
-    public void Hide()
-    {
-        _animationHandler.ScreenFadeOut();
-    }
-
-    private void OnButtonClick()
-    {
-        _yandexAds.ShowRewardedVideo();
-        Hide();
-    } 
+    public void Show() => _yandexAds.ShowRewardedVideo();
     
-    private void OnRewardedVideoCallBack()
-    {
-        Hide();
-        _commandExecuter.ResetAbilityOnReward();
-    }
+    private void OnRewardedVideoCallBack() => _commandExecuter.ResetAbilityOnReward();
 }
