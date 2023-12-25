3ba7b199fb30d1573efaee7f35cf0d5c4084045e
Arphail <Alexusoffd@gmail.com>
2023-12-25 05:43:35 +0300

    DialogueHandler and interactive object merged succesfully

--- Assets/Source/DialogueLine/DialogueHandler.cs
+++ Assets/Source/DialogueLine/DialogueHandler.cs
@@ -1,26 +1,24 @@
 using System.Collections;
 using UnityEngine;
+using System;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
 public class DialogueHandler : MonoBehaviour
 {
+    [SerializeField] private TMPro.TMP_Text _textContainer;
     [SerializeField] private float _delay;
     [SerializeField] private float _startDelay;
-    [SerializeField] private TMPro.TMP_Text _referenceText;
-    [SerializeField] private TMPro.TMP_Text _textContainer;
-    [SerializeField] private AudioClip _voiceSampleBeep;
     [SerializeField] private float _minVoicePitch;
     [SerializeField] private float _maxVoicePitch;
+    [SerializeField] private AudioClip _voiceSampleBeep;
 
     private Coroutine _dialogueCoroutine;
     private WaitForSeconds _waitDelay;
     private WaitForSeconds _waitStartDelay;
     private AudioSource _source;
-    private char _space = ' ';
-    private char _comma = ',';
-    private char _dot = '.';
-    private char _explanetion = '!';
-    private char _question = '?';
 
     private void Start()
     {
@@ -29,7 +27,7 @@ public class DialogueHandler : MonoBehaviour
         _source = GetComponent<AudioSource>();
     }
      
-    public void WriteDialogue()
+    public void WriteDialogue(List<TMPro.TMP_Text> referenceTexts)
     {
         if(_dialogueCoroutine != null)
         {
@@ -38,30 +36,29 @@ public class DialogueHandler : MonoBehaviour
         }
 
         WipeText();
-        _dialogueCoroutine = StartCoroutine(WriteLine());
+        _dialogueCoroutine = StartCoroutine(WriteLine(referenceTexts));
     }
 
-    private IEnumerator WriteLine()
+    private IEnumerator WriteLine(List<TMPro.TMP_Text> referenceTexts)
     {
-        yield return _waitStartDelay;
-
-        for(int i = 0; i < _referenceText.text.Length; i++)
+        foreach(var referenceText in referenceTexts)
         {
-            _source.pitch = Random.Range(_minVoicePitch, _maxVoicePitch);
+            yield return _waitStartDelay;
+            WipeText();
 
-            if (_referenceText.text[i] != _space && 
-                _referenceText.text[i] != _comma && 
-                _referenceText.text[i] != _dot && 
-                _referenceText.text[i] != _question && 
-                _referenceText.text[i] != _explanetion
-                )
-                _source.PlayOneShot(_voiceSampleBeep);
+            for (int i = 0; i < referenceText.text.Length; i++)
+            {
+                _source.pitch = Random.Range(_minVoicePitch, _maxVoicePitch);
 
-            _textContainer.text += _referenceText.text[i];
-            yield return _waitDelay;
+                if (!char.IsPunctuation(referenceText.text[i]) && !char.IsWhiteSpace(referenceText.text[i]))
+                    _source.PlayOneShot(_voiceSampleBeep);
+
+                _textContainer.text += referenceText.text[i];
+                yield return _waitDelay;
+            }
+
+            _dialogueCoroutine = null;
         }
-
-        _dialogueCoroutine = null;
     }
 
     private void WipeText()
--- Assets/Source/InteractiveObjects/Objects/TutorialZone.cs
+++ Assets/Source/InteractiveObjects/Objects/TutorialZone.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class TutorialZone : MonoBehaviour
+public class TutorialZone : InteractiveObject
 {
-    
+    [SerializeField] private List<TMPro.TMP_Text> _referenceTexts;
+    [SerializeField] private DialogueHandler _dialogueHandler;
+
+    private bool _isExecuted;
+
+    public override void Interact()
+    {
+        _dialogueHandler.WriteDialogue(_referenceTexts);
+        _isExecuted = true;
+    }
+
+    public override void Prepare()
+    {
+        if (!_isExecuted)
+            if (CheckInteractionPossibility())
+                Interact();
+    }
+
+    protected override void Disable()
+    {
+        
+    }
 }
