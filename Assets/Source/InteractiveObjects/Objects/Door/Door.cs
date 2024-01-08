88e7370328713b9e71d4e3d761ff90a1315ee6a4
Arphail <alexusoffd@gmail.com>
2024-01-08 22:50:38 +0300

    Tutorial door sound ready

--- Assets/Source/InteractiveObjects/Objects/Door/Door.cs
+++ Assets/Source/InteractiveObjects/Objects/Door/Door.cs
@@ -1,13 +1,30 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class Door : MonoBehaviour
 {
+    [SerializeField] private AudioClip _open;
+    [SerializeField] private AudioClip _close;
+
     private Animator _animator;
+    private AudioSource _source;
 
-    private void Awake() => _animator = GetComponent<Animator>();
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _source = GetComponent<AudioSource>();
+    }
 
-    public void Open() => _animator.SetTrigger(Constants.OpenParameter);
+    public void Open()
+    {
+        _animator.SetTrigger(Constants.OpenParameter);
+        _source.PlayOneShot(_open);
+    }
 
-    public void Close() => _animator.SetTrigger(Constants.CloseParameter);
+    public void Close()
+    {
+        _animator.SetTrigger(Constants.CloseParameter);
+        _source.PlayOneShot(_close);
+    }
 }
