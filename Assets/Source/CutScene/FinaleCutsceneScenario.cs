6a79021b3e169cc7a815265142e39061283c3f65
Arphail <Alexusoffd@gmail.com>
2024-02-01 00:44:58 +0300

    Some finale cutscene stuff ready

--- Assets/Source/CutScene/FinaleCutsceneScenario.cs
+++ Assets/Source/CutScene/FinaleCutsceneScenario.cs
@@ -11,14 +11,20 @@ public class FinaleCutsceneScenario : MonoBehaviour
     [SerializeField] private float _swordFallingDuration;
     [SerializeField] private float _swordStabingDuration;
     [SerializeField] private float _screamDuration;
+    [SerializeField] private float _whiteScreenFadeDuration;
     [SerializeField] private float _effectFadeUpSpeed;
     [SerializeField] private float _cameraFrequencyFadeSpeed;
     [SerializeField] private float _cameraAmplitudeFadeSpeed;
+    [SerializeField] private float _soundFadeSpeed;
     [SerializeField] private float _cameraFrequencyFinalIntensity;
     [SerializeField] private float _cameraAmplitudeFinalIntensity;
-    [SerializeField] private float _whiteScreenFadeDuration;
     [SerializeField] private AudioSource _goreSource;
     [SerializeField] private AudioSource _screamSource;
+    [SerializeField] private AudioSource _music;
+    [SerializeField] private AudioSource _whiteNoise;
+    [SerializeField] private AudioSource _heartBeat;
+    [SerializeField] private AudioSource _whooshSource1;
+    [SerializeField] private AudioSource _whooshSource2;
     [SerializeField] private WhooshScript _whoosh;
     [SerializeField] private EffectChangeHandler _effectChanger;
     [SerializeField] private ParticleSystem _effect;
@@ -58,6 +64,12 @@ public class FinaleCutsceneScenario : MonoBehaviour
         _genericWait = new WaitForSeconds(_swordStabingDuration);
         yield return _genericWait;
         _screamSource.Play();
+        _whiteNoise.Play();
+        StartCoroutine(FadeSound(_music, 0));
+        StartCoroutine(FadeSound(_heartBeat, 0));
+        StartCoroutine(FadeSound(_whooshSource1, 0));
+        StartCoroutine(FadeSound(_whooshSource2, 0));
+        StartCoroutine(FadeSound(_whiteNoise, .5f));
         _genericWait = new WaitForSeconds(_screamDuration);
         yield return _genericWait;
         _whiteScreen.DOFade(1, _whiteScreenFadeDuration).SetEase(Ease.InSine);
@@ -71,6 +83,15 @@ public class FinaleCutsceneScenario : MonoBehaviour
         {
             _noise.m_FrequencyGain = Mathf.MoveTowards(_noise.m_FrequencyGain, frequencyTarget, _cameraFrequencyFadeSpeed * Time.deltaTime);
             _noise.m_AmplitudeGain = Mathf.MoveTowards(_noise.m_AmplitudeGain, amplitudeTarget, _cameraAmplitudeFadeSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeSound(AudioSource source, float target)
+    {
+        while(source.volume != target)
+        {
+            source.volume = Mathf.MoveTowards(source.volume, target, _soundFadeSpeed * Time.deltaTime);
             yield return null;
         }
     }
