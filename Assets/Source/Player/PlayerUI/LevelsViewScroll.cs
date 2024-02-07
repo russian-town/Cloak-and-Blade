00e08bcb2926066d0b5b694097d0c77edcb1abc9
Yuriy <russian_town_98@mail.ru>
2024-02-08 00:34:47 +0600

    Poop

--- Assets/Source/Player/PlayerUI/LevelsViewScroll.cs
+++ Assets/Source/Player/PlayerUI/LevelsViewScroll.cs
@@ -1,20 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class LevelsViewScroll : MonoBehaviour
 {
     [SerializeField] private ScreenAnimationHandler _screen;
-    [SerializeField] private Image _blackOut;
-    [SerializeField] private Scrollbar _scrollbar;
 
-    public void Show()
-    {
-        _scrollbar.value = 0;
-        _screen.FadeIn();
-    }
+    public void Show() => _screen.FadeIn();
 
-    public void Hide()
-    {
-        _screen.FadeOut();
-    }
+    public void Hide() => _screen.FadeOut();
 }
